9da64812695c46a5063a63832d91613201b682d9
Mariusz Barczak <mariusz.barczak@intel.com>
2019-08-06 14:55:10 +0200

    Traces device id in IO cpmpletion (#71)
    Signed-off-by: Mariusz Barczak <mariusz.barczak@intel.com>

--- source/kernel/trace_bio.c
+++ source/kernel/trace_bio.c
@@ -3,11 +3,11 @@
  * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
-#include <linux/blkdev.h>
 #include <linux/bio.h>
+#include <linux/blkdev.h>
+#include "config.h"
 #include "context.h"
 #include "iotrace_event.h"
-#include "config.h"
 
 /**
  * @note IO classification defined by Differentiated Storage Services (DSS)
@@ -33,52 +33,50 @@ enum {
 };
 
 /* DSS: tagging according to file sizes */
-static inline int _file_size_to_io_class(const struct inode *inode)
-{
-	int tag = DSS_MISC;
+static inline int _file_size_to_io_class(const struct inode *inode) {
+    int tag = DSS_MISC;
 
-	if (!inode) {
-		printk(KERN_ERR "No INODE\n");
-		return tag;
-	}
+    if (!inode) {
+        printk(KERN_ERR "No INODE\n");
+        return tag;
+    }
 
-	/* DSS.4 (note that inode 8 (the journal) passes through here) */
-	if (S_ISREG(inode->i_mode)) {
-		/* file size classification */
-		loff_t size = i_size_read(inode);
+    /* DSS.4 (note that inode 8 (the journal) passes through here) */
+    if (S_ISREG(inode->i_mode)) {
+        /* file size classification */
+        loff_t size = i_size_read(inode);
 
-		/* TODO get rid of assembler code */
+        /* TODO get rid of assembler code */
 #ifdef CONFIG_X86_64
-		__asm__("movq %1, %%rax;"
-			"subq $1, %%rax;"
-			"orq $(4095), %%rax;"
-			"bsrq %%rax, %%rax;"
-			"shrl $1, %%eax;"
-			"addl $6, %%eax;"
-			"movl $21, %%ecx;"
-			"cmpl %%ecx, %%eax;"
-			"cmovgel %%ecx, %%eax;"
-			"movl %%eax, %0"
-			: "=r"(tag)
-			: "r"(size)
-			: "%rax", "%rcx");
+        __asm__("movq %1, %%rax;"
+                "subq $1, %%rax;"
+                "orq $(4095), %%rax;"
+                "bsrq %%rax, %%rax;"
+                "shrl $1, %%eax;"
+                "addl $6, %%eax;"
+                "movl $21, %%ecx;"
+                "cmpl %%ecx, %%eax;"
+                "cmovgel %%ecx, %%eax;"
+                "movl %%eax, %0"
+                : "=r"(tag)
+                : "r"(size)
+                : "%rax", "%rcx");
 #else
-		if (size <= 1024 * 1048576) {
-			unsigned int bits =
-				__builtin_clzl((size - 1) | (4096 - 1));
+        if (size <= 1024 * 1048576) {
+            unsigned int bits = __builtin_clzl((size - 1) | (4096 - 1));
 
-			tag = 64 - bits + DSS_DATA_FILE_4KB;
-			tag >>= 1;
-		} else {
-			tag = DSS_DATA_FILE_BULK;
-		}
+            tag = 64 - bits + DSS_DATA_FILE_4KB;
+            tag >>= 1;
+        } else {
+            tag = DSS_DATA_FILE_BULK;
+        }
 #endif
-	} else if (S_ISDIR(inode->i_mode)) {
-		tag = DSS_DATA_DIR;
-	} else {
-		tag = DSS_MISC;
-	}
-	return tag;
+    } else if (S_ISDIR(inode->i_mode)) {
+        tag = DSS_DATA_DIR;
+    } else {
+        tag = DSS_MISC;
+    }
+    return tag;
 }
 
 struct bio_info {
@@ -86,62 +84,63 @@ struct bio_info {
     const struct page *page;
 };
 
-static uint32_t _get_dss_io_class(const struct bio *bio, struct bio_info *info)
-{
-	/* Try get inode of block device */
-	struct inode *bio_inode = NULL;
-	struct page *page = NULL;
+static uint32_t _get_dss_io_class(const struct bio *bio,
+                                  struct bio_info *info) {
+    /* Try get inode of block device */
+    struct inode *bio_inode = NULL;
+    struct page *page = NULL;
 
-	if (!bio)
-		return DSS_UNCLASSIFIED;
+    if (!bio)
+        return DSS_UNCLASSIFIED;
 
-	page = bio_page(bio);
+    page = bio_page(bio);
 
-	if (!page)
-		return DSS_UNCLASSIFIED;
+    if (!page)
+        return DSS_UNCLASSIFIED;
 
-	if (PageAnon(page))
-		return DSS_DATA_DIRECT;
+    if (PageAnon(page))
+        return DSS_DATA_DIRECT;
 
-	if (PageSlab(page) || PageCompound(page)) {
-		/* A filesystem issues IO on pages that does not belongs
-		 * to the file page cache. It means that it is a
-		 * part of metadata
-		 */
-		return DSS_METADATA;
-	}
+    if (PageSlab(page) || PageCompound(page)) {
+        /* A filesystem issues IO on pages that does not belongs
+         * to the file page cache. It means that it is a
+         * part of metadata
+         */
+        return DSS_METADATA;
+    }
 
-	if (!page->mapping) {
-		/* XFS case, pages are allocated internally and do not
-		 * have references into inode
-		 */
-		return DSS_METADATA;
-	}
+    if (!page->mapping) {
+        /* XFS case, pages are allocated internally and do not
+         * have references into inode
+         */
+        return DSS_METADATA;
+    }
 
-	bio_inode = page->mapping->host;
-	if (!bio_inode)
-		return DSS_UNCLASSIFIED;
+    bio_inode = page->mapping->host;
+    if (!bio_inode)
+        return DSS_UNCLASSIFIED;
 
-	if (S_ISBLK(bio_inode->i_mode)) {
-		/* EXT3 and EXT4 case. Metadata IO is performed into pages
-		 * of block device cache
-		 */
-		return DSS_METADATA;
-	}
+    if (S_ISBLK(bio_inode->i_mode)) {
+        /* EXT3 and EXT4 case. Metadata IO is performed into pages
+         * of block device cache
+         */
+        return DSS_METADATA;
+    }
 
-	info->page = page;
-	info->inode = bio_inode;
+    info->page = page;
+    info->inode = bio_inode;
 
-	return _file_size_to_io_class(bio_inode);
+    return _file_size_to_io_class(bio_inode);
 }
 
-static void _trace_bio_fs_meta(octf_trace_t trace, log_sid_t sid, log_sid_t ref_sid,
-        struct bio_info* info)
-{
+static void _trace_bio_fs_meta(octf_trace_t trace,
+                               log_sid_t sid,
+                               log_sid_t ref_sid,
+                               struct bio_info *info) {
     struct iotrace_event_fs_meta ev = {};
 
-    iotrace_event_init_hdr(&ev.hdr, iotrace_event_type_fs_meta,
-            sid, ktime_to_ns(ktime_get()), sizeof(ev));
+    iotrace_event_init_hdr(&ev.hdr, iotrace_event_type_fs_meta, sid,
+                           ktime_to_ns(ktime_get()), sizeof(ev));
 
     ev.ref_sid = ref_sid;
     ev.file_id = info->inode->i_ino;
@@ -151,9 +150,10 @@ static void _trace_bio_fs_meta(octf_trace_t trace, log_sid_t sid, log_sid_t ref_
     octf_trace_push(trace, &ev, sizeof(ev));
 }
 
-void iotrace_trace_bio(struct iotrace_context *context, unsigned cpu,
-                  uint64_t dev_id, struct bio *bio)
-{
+void iotrace_trace_bio(struct iotrace_context *context,
+                       unsigned cpu,
+                       uint64_t dev_id,
+                       struct bio *bio) {
     struct iotrace_event ev = {};
     struct iotrace_state *state = &context->trace_state;
     uint64_t sid = atomic64_inc_return(&state->sid);
@@ -161,7 +161,7 @@ void iotrace_trace_bio(struct iotrace_context *context, unsigned cpu,
     octf_trace_t trace = *per_cpu_ptr(state->traces, cpu);
 
     iotrace_event_init_hdr(&ev.hdr, iotrace_event_type_io, sid,
-              ktime_to_ns(ktime_get()), sizeof(ev));
+                           ktime_to_ns(ktime_get()), sizeof(ev));
 
     if (IOTRACE_BIO_IS_DISCARD(bio))
         ev.operation = iotrace_event_operation_discard;
@@ -189,21 +189,23 @@ void iotrace_trace_bio(struct iotrace_context *context, unsigned cpu,
         _trace_bio_fs_meta(trace, atomic64_inc_return(&state->sid), sid, &info);
 }
 
-void iotrace_trace_bio_completion(struct iotrace_context *context, unsigned cpu,
-        uint64_t dev_id, struct bio *bio, int error)
-{
+void iotrace_trace_bio_completion(struct iotrace_context *context,
+                                  unsigned cpu,
+                                  uint64_t dev_id,
+                                  struct bio *bio,
+                                  int error) {
     struct iotrace_event_completion cmpl = {};
     struct iotrace_state *state = &context->trace_state;
     uint64_t sid = atomic64_inc_return(&state->sid);
     octf_trace_t trace = *per_cpu_ptr(state->traces, cpu);
 
     iotrace_event_init_hdr(&cmpl.hdr, iotrace_event_type_io_cmpl, sid,
-              ktime_to_ns(ktime_get()), sizeof(cmpl));
-
+                           ktime_to_ns(ktime_get()), sizeof(cmpl));
 
     cmpl.lba = IOTRACE_BIO_BISECTOR(bio);
     cmpl.len = IOTRACE_BIO_BISIZE(bio) >> SECTOR_SHIFT;
     cmpl.error = error;
+    cmpl.dev_id = dev_id;
 
     octf_trace_push(trace, &cmpl, sizeof(cmpl));
 }
