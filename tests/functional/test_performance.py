e8326bae667c4cfbd28da5a7e9f4a233d0ec5e74
mmkayPL <mateusz.kozlowski@intel.com>
2019-12-17 11:14:23 +0100

    Add performance test for tracing during workload (#124)
    Signed-off-by: Mateusz Kozlowski <mateusz.kozlowski@intel.com>

---
+++ tests/functional/test_performance.py
@@ -0,0 +1,36 @@
+#
+# Copyright(c) 2019 Intel Corporation
+# SPDX-License-Identifier: BSD-3-Clause-Clear
+#
+
+import datetime
+
+from core.test_run import TestRun
+from iotrace import IotracePlugin
+from utils.fio import run_workload
+from test_tools.fio.fio_param import ReadWrite
+
+
+def test_data_performance_20s():
+    TestRun.LOGGER.info("Testing performance drop with tracing")
+    iotrace: IotracePlugin = TestRun.plugins['iotrace']
+    runtime = datetime.timedelta(seconds=20)
+    jobs = 16
+    method = ReadWrite.randread
+
+    for disk in TestRun.dut.disks:
+        with TestRun.step("Run test random read workload without tracing"):
+            results = run_workload(disk.system_path, runtime, verify=False, num_jobs=jobs, method=method)
+            clean_iops = sum(job.read_iops() for job in results)
+            TestRun.LOGGER.info("Recorded " + str(clean_iops) + " IOPS without tracing")
+        with TestRun.step("Start tracing"):
+            iotrace.start_tracing([disk.system_path])
+        with TestRun.step("Run test random read workload with tracing"):
+            trace = run_workload(disk.system_path, runtime, verify=False, num_jobs=jobs, method=method)
+            trace_iops = sum(job.read_iops() for job in trace)
+            TestRun.LOGGER.info("Recorded " + str(trace_iops) + " IOPS with tracing")
+        with TestRun.step("Stop tracing"):
+            iotrace.stop_tracing()
+
+        if trace_iops / clean_iops < 0.98:
+            raise Exception("Excessive performance drop during tracing")
--- tests/utils/fio.py
+++ tests/utils/fio.py
@@ -3,12 +3,14 @@
 # SPDX-License-Identifier: BSD-3-Clause-Clear
 #
 
+from core.test_run import TestRun
 from test_tools.fio.fio import Fio
 from test_tools.fio.fio_param import ReadWrite, IoEngine, VerifyMethod
 from test_utils.size import Unit, Size
 
 
-def run_workload(target, runtime, io_depth=128, verify=True, block_size=int(Size(4, Unit.KibiByte)), num_jobs=1):
+def run_workload(target, runtime, io_depth=128, verify=True,
+                 block_size=int(Size(4, Unit.KibiByte)), num_jobs=1, method=ReadWrite.randrw):
     fio_run = Fio().create_command()
     fio_run.io_engine(IoEngine.libaio)
     fio_run.direct()
@@ -21,11 +23,11 @@ def run_workload(target, runtime, io_depth=128, verify=True, block_size=int(Size
         fio_run.verify(VerifyMethod.meta)
         fio_run.verify_dump()
 
-    fio_run.read_write(ReadWrite.randrw)
+    fio_run.read_write(method)
     fio_run.target(target)
     fio_run.block_size(block_size)
 
     for i in range(num_jobs):
         fio_run.add_job()
 
-    fio_run.run()
+    return fio_run.run()
