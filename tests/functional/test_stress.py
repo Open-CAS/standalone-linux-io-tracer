b601237dfcd5077eda68fca3f7800cc91db5b809
Sławomir Jankowski <slawomir.jankowski@intel.com>
2020-03-06 14:01:21 +0100

    Add stress test for iotracer starting and stopping (#167)
    Signed-off-by: Slawomir Jankowski <slawomir.jankowski@intel.com>

---
+++ tests/functional/test_stress.py
@@ -0,0 +1,31 @@
+#
+# Copyright(c) 2020 Intel Corporation
+# SPDX-License-Identifier: BSD-3-Clause-Clear
+#
+
+from core.test_run import TestRun
+from utils.iotrace import IotracePlugin
+
+iterations = 50
+
+
+def test_stress_iotracer():
+    """
+        title: Stress test for starting and stopping io-tracer.
+        description: |
+          Validate the ability of io-tracer to start and stop
+          quickly in the loop.
+        pass_criteria:
+          - No system crash.
+          - Io-tracer can handle many quick starts and stops.
+    """
+    with TestRun.step("Prepare io-tracer."):
+        iotrace: IotracePlugin = TestRun.plugins["iotrace"]
+
+    for _ in TestRun.iteration(
+            range(0, iterations), f"Start and stop CAS {iterations} times."):
+        with TestRun.step("Start io-tracer."):
+            iotrace.start_tracing()
+
+        with TestRun.step("Stop io-tracer."):
+            iotrace.stop_tracing()
