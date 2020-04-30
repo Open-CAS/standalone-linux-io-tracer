2f92ca7ee68965e9d5e1892f8cabad3917cc9912
Mateusz Kozlowski <mateusz.kozlowski@intel.com>
2020-04-30 06:44:27 -0400

    Syntax error

--- tests/utils/iotrace.py
+++ tests/utils/iotrace.py
@@ -176,7 +176,7 @@ class IotracePlugin:
             TestRun.LOGGER.info("Iotrace processes not found.")
             return False
 
-        elif self.pid is not None self.pid not in output.stdout:
+        elif self.pid is not None and self.pid not in output.stdout:
             TestRun.LOGGER.info(f"Found other iotrace process with PID {output.stdout}")
             return False
 
--- tests/utils/misc.py
+++ tests/utils/misc.py
@@ -10,4 +10,4 @@ def kill_all_io():
     TestRun.executor.run("pkill --signal SIGKILL dd")
     TestRun.executor.run(
         "kill -9 `ps aux | grep -i vdbench.* | awk '{ print $1 }'`")
-    TestRun.executor.run("pkill --signal SIGKILL fio*")
+    TestRun.executor.run("pkill --signal SIGKILL 'fio*'")
